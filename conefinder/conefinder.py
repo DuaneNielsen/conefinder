0281cd9108929b780a6ac0735cb7dc26d51c0d18
duane <duane.nielsen.rocks@gmail.com>
2022-12-29 17:25:59 -0800

    Get rid of torch code and dependences on yolov7.

--- conefinder/conefinder.py
+++ conefinder/conefinder.py
@@ -13,11 +13,11 @@ from pathlib import Path
 """
 expects yolov7 repo in home directory for pytorch
 """
-sys.path.insert(0, str(Path.home()) + '/yolov7')
-from models.experimental import attempt_load
-from utils.general import check_img_size, non_max_suppression, scale_coords, xyxy2xywh
-from utils.torch_utils import select_device
-from utils.datasets import letterbox
+# sys.path.insert(0, str(Path.home()) + '/yolov7')
+# from models.experimental import attempt_load
+# from utils.general import check_img_size, non_max_suppression, scale_coords, xyxy2xywh
+# from utils.torch_utils import select_device
+# from utils.datasets import letterbox
 
 """
 expects YOLOv7_Tensorrt in home directory for Tensor RT
@@ -28,107 +28,107 @@ from datetime import datetime
 
 timers = False
 
-
-def img_preprocess(img, device, half, net_size):
-    net_image, ratio, pad = letterbox(img[:, :, :3], net_size, auto=False)
-    net_image = net_image.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
-    net_image = np.ascontiguousarray(net_image)
-
-    img = torch.from_numpy(net_image).to(device)
-    img = img.half() if half else img.float()  # uint8 to fp16/32
-    img /= 255.0  # 0 - 255 to 0.0 - 1.0
-
-    if img.ndimension() == 3:
-        img = img.unsqueeze(0)
-    return img, ratio, pad
-
-
-def xywh2abcd(xywh, im_shape):
-    output = np.zeros((4, 2))
-
-    # Center / Width / Height -> BBox corners coordinates
-    x_min = (xywh[0] - 0.5 * xywh[2]) * im_shape[1]
-    x_max = (xywh[0] + 0.5 * xywh[2]) * im_shape[1]
-    y_min = (xywh[1] - 0.5 * xywh[3]) * im_shape[0]
-    y_max = (xywh[1] + 0.5 * xywh[3]) * im_shape[0]
-
-    # A ------ B
-    # | Object |
-    # D ------ C
-
-    output[0][0] = x_min
-    output[0][1] = y_min
-
-    output[1][0] = x_max
-    output[1][1] = y_min
-
-    output[2][0] = x_min
-    output[2][1] = y_max
-
-    output[3][0] = x_max
-    output[3][1] = y_max
-    return output
-
-
-def detections_to_custom_box(detections, im, im0):
-    output = []
-    for i, det in enumerate(detections):
-        if len(det):
-            det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-
-            for *xyxy, conf, cls in reversed(det):
-                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-
-                # Creating ingestable objects for the ZED SDK
-                obj = sl.CustomBoxObjectData()
-                obj.bounding_box_2d = xywh2abcd(xywh, im0.shape)
-                obj.label = cls
-                obj.probability = conf
-                obj.is_grounded = False
-                output.append(obj)
-    return output
-
-
-def torch_thread(weights, img_size, conf_thres=0.2, iou_thres=0.45):
-    global image_net, exit_signal, run_signal, detections, detector_loaded, detector_exited
-
-    print("Intializing Network...")
-
-    device = select_device()
-    half = device.type != 'cpu'  # half precision only supported on CUDA
-    imgsz = img_size
-
-    # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32
-    stride = int(model.stride.max())  # model stride
-    imgsz = check_img_size(imgsz, s=stride)  # check img_size
-    if half:
-        model.half()  # to FP16
-    cudnn.benchmark = True
-
-    # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-
-    detector_loaded.set()
-
-    while not exit_signal:
-        if run_signal:
-            lock.acquire()
-            img, ratio, pad = img_preprocess(image_net, device, half, imgsz)
-
-            pred = model(img)[0]
-            det = non_max_suppression(pred, conf_thres, iou_thres)
-
-            # ZED CustomBox format (with inverse letterboxing tf applied)
-            detections = detections_to_custom_box(det, img, image_net)
-            lock.release()
-            run_signal = False
-        sleep(0.01)
-
-    detector_exited.set()
-    print('torch exited')
+#
+# def img_preprocess(img, device, half, net_size):
+#     net_image, ratio, pad = letterbox(img[:, :, :3], net_size, auto=False)
+#     net_image = net_image.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+#     net_image = np.ascontiguousarray(net_image)
+#
+#     img = torch.from_numpy(net_image).to(device)
+#     img = img.half() if half else img.float()  # uint8 to fp16/32
+#     img /= 255.0  # 0 - 255 to 0.0 - 1.0
+#
+#     if img.ndimension() == 3:
+#         img = img.unsqueeze(0)
+#     return img, ratio, pad
+#
+#
+# def xywh2abcd(xywh, im_shape):
+#     output = np.zeros((4, 2))
+#
+#     # Center / Width / Height -> BBox corners coordinates
+#     x_min = (xywh[0] - 0.5 * xywh[2]) * im_shape[1]
+#     x_max = (xywh[0] + 0.5 * xywh[2]) * im_shape[1]
+#     y_min = (xywh[1] - 0.5 * xywh[3]) * im_shape[0]
+#     y_max = (xywh[1] + 0.5 * xywh[3]) * im_shape[0]
+#
+#     # A ------ B
+#     # | Object |
+#     # D ------ C
+#
+#     output[0][0] = x_min
+#     output[0][1] = y_min
+#
+#     output[1][0] = x_max
+#     output[1][1] = y_min
+#
+#     output[2][0] = x_min
+#     output[2][1] = y_max
+#
+#     output[3][0] = x_max
+#     output[3][1] = y_max
+#     return output
+#
+#
+# def detections_to_custom_box(detections, im, im0):
+#     output = []
+#     for i, det in enumerate(detections):
+#         if len(det):
+#             det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+#             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+#
+#             for *xyxy, conf, cls in reversed(det):
+#                 xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+#
+#                 # Creating ingestable objects for the ZED SDK
+#                 obj = sl.CustomBoxObjectData()
+#                 obj.bounding_box_2d = xywh2abcd(xywh, im0.shape)
+#                 obj.label = cls
+#                 obj.probability = conf
+#                 obj.is_grounded = False
+#                 output.append(obj)
+#     return output
+#
+#
+# def torch_thread(weights, img_size, conf_thres=0.2, iou_thres=0.45):
+#     global image_net, exit_signal, run_signal, detections, detector_loaded, detector_exited
+#
+#     print("Intializing Network...")
+#
+#     device = select_device()
+#     half = device.type != 'cpu'  # half precision only supported on CUDA
+#     imgsz = img_size
+#
+#     # Load model
+#     model = attempt_load(weights, map_location=device)  # load FP32
+#     stride = int(model.stride.max())  # model stride
+#     imgsz = check_img_size(imgsz, s=stride)  # check img_size
+#     if half:
+#         model.half()  # to FP16
+#     cudnn.benchmark = True
+#
+#     # Run inference
+#     if device.type != 'cpu':
+#         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+#
+#     detector_loaded.set()
+#
+#     while not exit_signal:
+#         if run_signal:
+#             lock.acquire()
+#             img, ratio, pad = img_preprocess(image_net, device, half, imgsz)
+#
+#             pred = model(img)[0]
+#             det = non_max_suppression(pred, conf_thres, iou_thres)
+#
+#             # ZED CustomBox format (with inverse letterboxing tf applied)
+#             detections = detections_to_custom_box(det, img, image_net)
+#             lock.release()
+#             run_signal = False
+#         sleep(0.01)
+#
+#     detector_exited.set()
+#     print('torch exited')
 
 
 def xyxy2abcd(x_min, y_min, x_max, y_max):
